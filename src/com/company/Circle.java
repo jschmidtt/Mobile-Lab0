979a0908c0c91d47de2fe7f7c68b4b1081b5e167
justinschmidt <justinschmidttt@gmail.com>
2018-09-05 10:14:22 -0400

    Finished the circle subclass of shape.

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -5,4 +5,20 @@ public class Circle extends Shape {
     public Circle(String name) {
         super(name);
     }
+
+    private double radius = 0.0;
+
+    public void setDimensions(double r){
+        radius = r;
+    }
+
+    @Override
+    public void printDimensions(){
+        System.out.println(getName() + "radius is: "+ radius);
+    }
+
+    @Override
+    public double getArea(){
+        return (3.14 * (Math.pow(radius,2)));
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,16 +9,26 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         Shape square = new Square("Square One");
+        Shape circle = new Circle("Circle One");
 
         System.out.println("Please enter length and height for "+ square.getName());
         double l = input.nextDouble();
         double h = input.nextDouble();
         ((Square) square).setDimensions(l,h);
 
+        System.out.println("Please enter radius for "+ circle.getName());
+        double r = input.nextDouble();
+        ((Circle) circle).setDimensions(r);
+
+        //Preform Square Ops and Print
         System.out.println(square.getName());
         square.printDimensions();
-        System.out.println("The area " + square.getArea());
+        System.out.println("The area " + square.getArea() +"\n");
 
+        //Preform Circle Ops and Print
+        System.out.println(circle.getName());
+        circle.printDimensions();
+        System.out.println("The area " + circle.getArea());
 
     }
 }
